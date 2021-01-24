e79d995e2a7beaf8bc62b466af47a9e5726644c1
Patrycja <ulatowska.p@gmail.com>
2021-01-24 20:31:57 +0100

    a lot of changes

--- copywriting.js
+++ copywriting.js
@@ -12,15 +12,15 @@ function showArticles() {
   mainCopy.innerHTML = `<div>
   <span>Zoptymalizowane pod SEO</span>
   <br>
-  <span>Unikalne</span>
+  <span>Unikatowe teksty</span>
   <br>
-  <span>Poprawne ortograficznie i gramatycznie</span>
+  <span>Na każdy temat</span>
   <br>
-  <span>Merytoryczne</span>
+  <span>Profesjonalne treści</span>
   <br>
   <br>
   <br>
-  <a href="#">Zobacz przykładowe artykuły.</a>
+  <a href="artykuły.html">Zobacz przykładowe artykuły.</a>
   </div>`;
   mainCopy.style.display = "block";
   list.style.display = "none";
@@ -28,7 +28,23 @@ function showArticles() {
   title.innerHTML = "Artykuły";
 
   if (window.innerWidth > 960) {
-    mainCopy.innerHTML = "artykuły";
+    mainCopy.innerHTML = `
+    <h2>profesjonalne artykuły, na każdy zlecony temat. atrakcyjna oferta. merytoryczne treści. terminowość. kreatywność. zgodność z zasadami SEO</h2>
+    <div class="gallery">
+    <img src="images/articles/art1.png"/>
+    <img src="images/articles/art2.png"/>
+    <img src="images/articles/art3.png"/>
+    <img src="images/articles/art4.png"/>
+    <img src="images/articles/art5.png"/>
+    <img src="images/articles/art6.png"/>
+    </div>
+    `;
+
+    articles.style.textDecoration = "underline";
+    products.style.textDecoration = "none";
+    www.style.textDecoration = "none";
+    ads.style.textDecoration = "none";
+    contact.style.textDecoration = "none";
     list.style.display = "block";
     closeEv.style.display = "none";
     title.innerHTML = "Copywriting.";
@@ -49,17 +65,17 @@ closeEv.addEventListener("click", closeMain);
 
 function showWww() {
   mainCopy.innerHTML = `<div>
-  <span>Zoptymalizowane pod SEO</span>
+  <span>Treści angażujące klientów</span>
   <br>
-  <span>Unikalne</span>
+  <span>Elastyczność</span>
   <br>
-  <span>Poprawne ortograficznie i gramatycznie</span>
+  <span>Zgodne z zasadami SEO</span>
   <br>
   <span>Merytoryczne</span>
   <br>
   <br>
   <br>
-  <a href="#">Zobacz przykładowe opisy na stronach.</a>
+  <a href="www.html">Zobacz przykładowe opisy na stronach.</a>
   </div>`;
   mainCopy.style.display = "block";
   list.style.display = "none";
@@ -67,9 +83,52 @@ function showWww() {
   title.innerHTML = "Treści na stronach";
   if (window.innerWidth > 960) {
     list.style.display = "block";
+    articles.style.textDecoration = "none";
+    products.style.textDecoration = "none";
+    www.style.textDecoration = "underline";
+    ads.style.textDecoration = "none";
+    contact.style.textDecoration = "none";
     closeEv.style.display = "none";
     title.innerHTML = "Copywriting.";
-    mainCopy.innerHTML = "strony www";
+    mainCopy.innerHTML = `
+    <h2>treści, które angażują klienta. merytoryczność. elastyczność. SEO.</h2>
+    <div class="small_gallery gallery-www">
+    <div id="item1"><img src="images/www/www1.png"/></div>
+    <div id="item4"><img src="images/www/www4.png"/></div>
+    <div id="item5"><img src="images/www/www5.png"/></div>
+    </div> 
+    <div id="preview">
+    </div>
+    `;
+
+    document.getElementById("item1").onmouseover = function () {
+      document.getElementById("preview").style.backgroundImage =
+        "url(images/www/www1.png)";
+      document.getElementById("preview").style.backgroundSize = "contain";
+      document.getElementById("preview").style.backgroundRepeat = "no-repeat";
+    };
+    document.getElementById("item1").onmouseout = function () {
+      document.getElementById("preview").style.backgroundImage = "";
+    };
+    document.getElementById("item4").onmouseover = function () {
+      document.getElementById("preview").style.backgroundImage =
+        "url(images/www/www4.png)";
+      document.getElementById("preview").style.backgroundSize = "contain";
+      document.getElementById("preview").style.backgroundRepeat = "no-repeat";
+    };
+    document.getElementById("item4").onmouseout = function () {
+      document.getElementById("preview").style.backgroundImage = "";
+    };
+
+    document.getElementById("item5").onmouseover = function () {
+      document.getElementById("preview").style.backgroundImage =
+        "url(images/www/www5.png)";
+      document.getElementById("preview").style.backgroundSize = "contain";
+      document.getElementById("preview").style.backgroundRepeat = "no-repeat";
+    };
+    document.getElementById("item5").onmouseout = function () {
+      document.getElementById("preview").style.backgroundImage = "";
+    };
   }
 }
 
@@ -98,9 +157,89 @@ function showProducts() {
   title.innerHTML = "Opisy produktów";
   if (window.innerWidth > 960) {
     list.style.display = "block";
+    articles.style.textDecoration = "none";
+    products.style.textDecoration = "underline";
+    www.style.textDecoration = "none";
+    ads.style.textDecoration = "none";
+    contact.style.textDecoration = "none";
     closeEv.style.display = "none";
     title.innerHTML = "Copywriting.";
-    mainCopy.innerHTML = "produkty";
+    mainCopy.innerHTML = `
+    <h2>zgodność z zasadami SEO. różnorodna tematyka. poprawność gramatyczna i ortograficzna. zachęcające teksty. </h2>
+    <div class="small_gallery">
+<div id="item1"><img src="images/png/ceiling-lamp.png"/></div>
+<div id="item2"><img src="images/png/cleaning.png"/></div>
+<div id="item3"><img src="images/png/wall-lamp.png"/></div>
+<div id="item4"><img src="images/png/liquid-soap.png"/></div>
+<div id="item5"><img src="images/png/hair-spray.png"/></div>
+<div id="item6"><img src="images/png/skincare.png"/></div>
+<div id="item7"><img src="images/png/skin-care.png"/></div>
+</div>
+
+<div id="preview">
+</div>
+`;
+
+    document.getElementById("item1").onmouseover = function () {
+      document.getElementById("preview").style.backgroundImage =
+        "url(images/produkty/opis1.png)";
+      document.getElementById("preview").style.backgroundSize = "contain";
+      document.getElementById("preview").style.backgroundRepeat = "no-repeat";
+    };
+    document.getElementById("item1").onmouseout = function () {
+      document.getElementById("preview").style.backgroundImage = "";
+    };
+
+    document.getElementById("item2").onmouseover = function () {
+      document.getElementById("preview").style.backgroundImage =
+        "url(images/produkty/opis2.png)";
+      document.getElementById("preview").style.backgroundSize = "contain";
+      document.getElementById("preview").style.backgroundRepeat = "no-repeat";
+    };
+    document.getElementById("item2").onmouseout = function () {
+      document.getElementById("preview").style.backgroundImage = "";
+    };
+
+    document.getElementById("item3").onmouseover = function () {
+      document.getElementById("preview").style.backgroundImage =
+        "url(images/produkty/opis3.png)";
+      document.getElementById("preview").style.backgroundSize = "contain";
+      document.getElementById("preview").style.backgroundRepeat = "no-repeat";
+    };
+    document.getElementById("item3").onmouseout = function () {
+      document.getElementById("preview").style.backgroundImage = "";
+    };
+
+    document.getElementById("item4").onmouseover = function () {
+      document.getElementById("preview").style.backgroundImage =
+        "url(images/produkty/opis4.png)";
+      document.getElementById("preview").style.backgroundSize = "contain";
+      document.getElementById("preview").style.backgroundRepeat = "no-repeat";
+    };
+    document.getElementById("item4").onmouseout = function () {
+      document.getElementById("preview").style.backgroundImage = "";
+    };
+
+    document.getElementById("item5").onmouseover = function () {
+      document.getElementById("preview").style.backgroundImage =
+        "url(images/produkty/opis5.png)";
+      document.getElementById("preview").style.backgroundSize = "contain";
+      document.getElementById("preview").style.backgroundRepeat = "no-repeat";
+    };
+
+    document.getElementById("item6").onmouseover = function () {
+      document.getElementById("preview").style.backgroundImage =
+        "url(images/produkty/opis6.png)";
+      document.getElementById("preview").style.backgroundSize = "contain";
+      document.getElementById("preview").style.backgroundRepeat = "no-repeat";
+    };
+
+    document.getElementById("item7").onmouseover = function () {
+      document.getElementById("preview").style.backgroundImage =
+        "url(images/produkty/opis7.png)";
+      document.getElementById("preview").style.backgroundSize = "contain";
+      document.getElementById("preview").style.backgroundRepeat = "no-repeat";
+    };
   }
 }
 
@@ -108,17 +247,17 @@ products.addEventListener("click", showProducts);
 
 function showAds() {
   mainCopy.innerHTML = `<div>
-  <span>Przyciągające uwagę</span>
+  <span>Przyciągające klientów</span>
   <br>
-  <span>Unikalne</span>
+  <span>Unikatowe</span>
   <br>
-  <span>Poprawne ortograficznie i gramatycznie</span>
+  <span>Kreatywne</span>
   <br>
-  <span>Merytoryczne</span>
+  <span>Bogate w słowa kluczowe</span>
   <br>
   <br>
   <br>
-  <a href="#">Zobacz przykładowe ogłoszenia.</a>
+  <a href="ogłoszenia.html">Zobacz przykładowe ogłoszenia.</a>
   </div>`;
   mainCopy.style.display = "block";
   list.style.display = "none";
@@ -126,9 +265,58 @@ function showAds() {
   title.innerHTML = "Ogłoszenia";
   if (window.innerWidth > 960) {
     list.style.display = "block";
+    articles.style.textDecoration = "none";
+    products.style.textDecoration = "none";
+    www.style.textDecoration = "none";
+    ads.style.textDecoration = "underline";
+    contact.style.textDecoration = "none";
     closeEv.style.display = "none";
     title.innerHTML = "Copywriting.";
-    mainCopy.innerHTML = "ogłoszenia";
+    mainCopy.innerHTML = `<h2>ogłoszenia. poprawa ogłoszeń na olx. większe zasięgi. więcej
+    klientów. bogate i merytoryczne treści reklamowe. zmień stronę i sprawdź, w jaki sposób ogłoszenie może wyglądać lepiej!</h2>
+    
+    <div id="dots"><span id="span1"></span><span id="span2"></span><span id="span3"></span></div>
+    <div class="flip-card">
+    <div class="flip-card-inner">
+    <div class="flip-card-front flip-card-front-one" id="frontbgc"></div>
+    <div class="flip-card-back flip-card-back-one" id="backbgc"></div>
+    </div>
+    </div>
+    </div>
+    `;
+    let dot1 = document.getElementById("span1");
+    let dot2 = document.getElementById("span2");
+    let dot3 = document.getElementById("span3");
+    let frontBgc = document.getElementById("frontbgc");
+    let backBgc = document.getElementById("backbgc");
+
+    function changeCard1() {
+      dot1.style.backgroundColor = "black";
+      dot2.style.backgroundColor = "white";
+      dot3.style.backgroundColor = "white";
+      frontBgc.style.backgroundImage = "url(images/ogłoszenia/buty1.png)";
+      backBgc.style.backgroundImage = "url(images/ogłoszenia/buty1a.png)";
+    }
+
+    function changeCard2() {
+      dot1.style.backgroundColor = "white";
+      dot2.style.backgroundColor = "black";
+      dot3.style.backgroundColor = "white";
+      frontBgc.style.backgroundImage = "url(images/ogłoszenia/fizyka1.png)";
+      backBgc.style.backgroundImage = "url(images/ogłoszenia/fizyka1a.png)";
+    }
+
+    function changeCard3() {
+      dot1.style.backgroundColor = "white";
+      dot2.style.backgroundColor = "white";
+      dot3.style.backgroundColor = "black";
+      frontBgc.style.backgroundImage = "url(images/ogłoszenia/paznokcie1.png)";
+      backBgc.style.backgroundImage = "url(images/ogłoszenia/paznokcie1a.png)";
+    }
+    dot1.style.backgroundColor = "black";
+    dot1.addEventListener("click", changeCard1);
+    dot2.addEventListener("click", changeCard2);
+    dot3.addEventListener("click", changeCard3);
   }
 }
 
@@ -148,9 +336,21 @@ function showContact() {
   title.innerHTML = "Kontakt";
   if (window.innerWidth > 960) {
     list.style.display = "block";
+    articles.style.textDecoration = "none";
+    products.style.textDecoration = "none";
+    www.style.textDecoration = "none";
+    ads.style.textDecoration = "none";
+    contact.style.textDecoration = "underline";
     closeEv.style.display = "none";
     title.innerHTML = "Copywriting.";
-    mainCopy.innerHTML = "kontakt";
+    mainCopy.innerHTML = `<h2>bezproblemowy kontakt. elastyczność. terminowość.</h2>
+    <div class="contact">
+  <span>Patrycja Ulatowska</span>
+  <br>
+  <span>692877781</span>
+  <br>
+  <span>ulatowska.p@gmail.com</span>
+  </div>`;
   }
 }
 
